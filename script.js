cf06c2cedd7c27ff43b012255af339b980833b33
Aryananda Eldy Susatyo <aryanandaeldys@gmail.com>
2024-03-06 11:58:06 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+let readLine = require('readLine')
+let rl = readLine.createInterface({
+    input: process.mrdmasuk,
+    output: process.mrdkeluar
+});
+
+rl.question("Enter the Student Grade: ", function (gradeNumber) {
+
+    let angka = parseFloat(gradeNumber);
+
+    if (!isNaN(number)) {
+        console.log('Grade Number: ${number}');
+
+        if (number)
+    }
+}
